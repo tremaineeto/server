823702975b9070ed17e3a36171e88aced0bc317e
aluhvihn <aluhvihn@ucla.edu>
2015-10-22 07:15:05 -0700

    Works for "404 Not Found" and "403 Forbidden", NOT for "200 OK"; Seg Fault when trying to access file_name
    1) I changed the parse() function to return file_name, since we don't know how big the file may be, so response_buffer might not be large enough. I was thinking of trying to write from the file directly to the socket, but I'm getting an error when trying to do 
    
    fopen(file_name, 'r')
    
    I tried using that command inside the actual parse() function, and it also seg faults there.
    
    2) The parse() function does return the filename correctly; I used a print statement to test and see.
    
    3) I also made rb_len a parameter to parse(), and returned the file size in the variable, to use for write() in the main() function

--- server.c
+++ server.c
@@ -18,7 +18,7 @@
 #include <string.h>
 #include <sys/time.h>
 #include <time.h>
-
+#include <sys/stat.h>
 
 /* REQUEST MESSAGE
  
@@ -81,104 +81,49 @@
 // http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
 // CTRL+F to "Enhancements to the server code" to see the dostuff() function to let the connection actually run forever
 
-void printSomething(){
-  printf("hello");
-}
-
-int get_word(int offset, char *line, char** word)
+int get_word(int offset, char* line, char* word)
 {
-    //memset(word,0,100);
+    memset(word, 0, 100);
     int position = 0;
     char c = line[offset+position];
     while (c != ' ' && c != '\r')  // the NULL is preventing this from being entered
     {
-        //printf("%c", c);
         c = line[offset+position];
         word[position] = c;
         position++;
     }
-    //printf("**** %s", word);
     return offset+position;
 }
 
-void parse(char *buffer, char** response_buffer)
+// Returns filesize if valid, -1 if not valid
+char *parse(char *buffer, char** response_buffer, int *rb_len)
 {
-    //printf("world");
-    char *line;
-    line = (char*) malloc(1000); 
+//     char *line;
+//     line = (char*) malloc(1000); 
 
-    line = strtok(buffer, "\n");
-
-    // printf("%c", line[0]); // G
-    // printf("%c", line[1]);
-    // printf("%c", line[2]);  // T
-    // printf("%c", line[3]);
-    // printf("%c", line[4]);  // /
-    // printf("%c", line[5]);  // t
-    // printf("%c", line[6]);
-    // printf("%c", line[7]);   // s
-    // printf("%c", line[8]);    // t
-    // printf("%c", line[9]);
-    // printf("%c", line[10]);   // h
-    // printf("%c", line[11]);
-    // printf("%c", line[12]);
-    // printf("%c", line[13]);
-    // printf("%c", line[14]);
-
-    // printf("%c", line[15]);   // H
-    // printf("%c", line[16]);
-    // printf("%c", line[17]);
-    // printf("%c", line[18]);   // P
-    // printf("%c", line[19]);   // / 
-    // printf("%c", line[20]);
-    // printf("%c", line[21]);
-    // printf("%c", line[22]);   // 1
-
+//     line = strtok(buffer, "\n");
+    
     // Request line
-    int word_start = 0, word_end = 0;
-    char req_type[100];
-    word_end = get_word(word_start, line, &req_type);
-    word_start = word_end + 1;
+//     int word_start = 0, word_end = 0;
+//     char req_type[100];
+//     word_end = get_word(word_start, line, &req_type);
+//     word_start = word_end + 1;
     
-    char file_name[100]; // has a slash as first char
-    word_end = get_word(word_start, line, &file_name);
-    word_start = word_end;
-    //memmove(file_name, file_name+1, strlen(file_name)); // should no longer have slash
+//     char file_name[100]; // has a slash as first char
+//     word_end = get_word(word_start, line, &file_name);
+//     word_start = word_end;
 
-    char html_version[100];
-    word_end = get_word(word_start, line, &html_version);
-
-    //printf("request: %s, file: %s, html version: %s", req_type, file_name, html_version);
+//     char html_version[100];
+//     word_end = get_word(word_start, line, &html_version);
     
+    char *req_type, *file_name, *html_version;
+    req_type = strtok(buffer, " ");
+    file_name = strtok(NULL, " ");
+    file_name++;    // Get rid of the slash in first char
+    html_version = strtok(NULL, "\r");
     
-    //     HTTP/1.0 404 Not Found
-    // Content-Type: text/html; charset=UTF-8
-    // X-Content-Type-Options: nosniff
-    // Date: Sun, 18 Oct 2015 01:29:40 GMT
-    // Server: sffe
-    // Content-Length: 1571
-    // X-XSS-Protection: 1; mode=block
-    
-    
-    // HTTP/1.1 404 Not Found
-    // Server: GitHub.com
-    // Date: Sun, 18 Oct 2015 01:36:46 GMT
-    // Content-Type: text/html; charset=utf-8
-    // Content-Length: 9116
-    // Connection: close
-    // ETag: "5519ee21-239c"
-    // Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline'; img-src data:; connect-src 'self'
-
-    // int j = 0;
-    // while(j < sizeof(file_name)){
-    //   printf("%c", file_name[j]);
-    //   j = j + 1;
-    // }
-
-    // OK, now we are correctly entering this when the file doesn't exist.
     if (access(file_name, F_OK) == -1)    //FILE DOESN'T EXIST
     {
-
         strncpy(*response_buffer, "HTTP/1.1", 8);    
         strcat(*response_buffer, " 404 Not Found\r\n");
         strcat(*response_buffer, "Server: CS118 Project\r\n");
@@ -188,10 +133,7 @@ void parse(char *buffer, char** response_buffer)
         struct tm * date_and_time;
         // date_and_time = localtime(&t);
         date_and_time = gmtime(&t);
-
         strcat(*response_buffer, "Date: ");
-
-        //printf("\n WEEKDAY INT: %d\n", date_and_time->tm_wday);
 
         // Day of the week
         switch (date_and_time->tm_wday) {
@@ -222,12 +164,10 @@ void parse(char *buffer, char** response_buffer)
         if (date_and_time->tm_mday < 10) {
             strcat(*response_buffer, "0");
         }
-
         char int_string[5];  // To convert integers to strings (year = need 5)
         int n;
         n = sprintf(int_string, "%d", date_and_time->tm_mday);
         strcat(*response_buffer, int_string);  // change second argument to string
-
       
         // Month
         switch (date_and_time->tm_mon) {
@@ -300,9 +240,9 @@ void parse(char *buffer, char** response_buffer)
 
         // Content Type
         int i;
-        for (i=0; i < sizeof(file_name); i+=sizeof(char*)) {
+        for (i=0; i < sizeof(file_name); i++) {
             if (file_name[i] == '.') {
-                switch (file_name[i+sizeof(char*)]) {
+                switch (file_name[i+1]) {
                 case 'j':
                     strcat(*response_buffer, "Content-Type: image/jpeg\r\n");
                     break;
@@ -316,7 +256,10 @@ void parse(char *buffer, char** response_buffer)
                 break;
             }
         }
-
+        if (i == sizeof(file_name)) {    // No extension
+            strcat(*response_buffer, "Content-Type: text/html; charset=utf-8\r\n");
+        }
+        
         // Content-Length
         strcat(*response_buffer, "Content-Length: 89\r\n");
         
@@ -325,14 +268,271 @@ void parse(char *buffer, char** response_buffer)
         
         // 404 Error Message
         strcat(*response_buffer, "\r\n<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD><BODY><H1>404 Not Found</H1></BODY></HTML>");
-
-        // printf("test");
-        printf("%s", *response_buffer);
+        return NULL;
     }
-
+    
     else  // Valid filename
     {
+        strncpy(*response_buffer, "HTTP/1.1", 8);
+        if (access(file_name, R_OK) == -1) {        // Permission Error
+            strcat(*response_buffer, " 403 Forbidden\r\n");
+        }
+        else {
+            strcat(*response_buffer, " 200 OK\r\n");
+        }
+        strcat(*response_buffer, "Connection: close\r\n");
+        strcat(*response_buffer, "Server: CS118 Project\r\n");
+
+        // Date and Time
+        time_t t = time(NULL);
+        struct tm * date_and_time;
+        // date_and_time = localtime(&t);
+        date_and_time = gmtime(&t);
+        strcat(*response_buffer, "Date: ");
+        // Day of the week
+        switch (date_and_time->tm_wday) {
+        case 0:
+            strcat(*response_buffer, "Sun, ");
+            break;
+        case 1:
+            strcat(*response_buffer, "Mon, ");
+            break;
+        case 2:
+            strcat(*response_buffer, "Tue, ");
+            break;
+        case 3:
+            strcat(*response_buffer, "Wed, ");
+            break;
+        case 4:
+            strcat(*response_buffer, "Thu, ");
+            break;
+        case 5:
+            strcat(*response_buffer, "Fri, ");
+            break;
+        case 6:
+            strcat(*response_buffer, "Sat, ");
+            break;
+        }
+        // Date of the month
+        if (date_and_time->tm_mday < 10) {
+            strcat(*response_buffer, "0");
+        }
+        char int_string[5];  // To convert integers to strings (year = need 5)
+        int n;
+        n = sprintf(int_string, "%d", date_and_time->tm_mday);
+        strcat(*response_buffer, int_string);  // change second argument to string
+        // Month
+        switch (date_and_time->tm_mon) {
+        case 0:
+            strcat(*response_buffer, " Jan ");
+            break;
+        case 1:
+            strcat(*response_buffer, " Feb ");
+            break;
+        case 2:
+            strcat(*response_buffer, " Mar ");
+            break;
+        case 3:
+            strcat(*response_buffer, " Apr ");
+            break;
+        case 4:
+            strcat(*response_buffer, " May ");
+            break;
+        case 5:
+            strcat(*response_buffer, " Jun ");
+            break;
+        case 6:
+            strcat(*response_buffer, " Jul ");
+            break;
+        case 7:
+            strcat(*response_buffer, " Aug ");
+            break;
+        case 8:
+            strcat(*response_buffer, " Sep ");
+            break;
+        case 9:
+            strcat(*response_buffer, " Oct ");
+            break;
+        case 10:
+            strcat(*response_buffer, " Nov ");
+            break;
+        case 11:
+            strcat(*response_buffer, " Dec ");
+            break;
+        }
+        // Year
+        n = sprintf(int_string, "%d", date_and_time->tm_year + 1900);  // Epoch time
+        strcat(*response_buffer, int_string);
+        // Hour
+        strcat(*response_buffer, " ");
+        if (date_and_time->tm_hour < 10) {
+            strcat(*response_buffer, "0");
+        }
+        n = sprintf(int_string, "%d", date_and_time->tm_hour);
+        strcat(*response_buffer, int_string);
+        // Minutes
+        strcat(*response_buffer, ":");
+        if (date_and_time->tm_min < 10) {
+            strcat(*response_buffer, "0");
+        }
+        n = sprintf(int_string, "%d", date_and_time->tm_min);
+        strcat(*response_buffer, int_string);
+        // Seconds
+        strcat(*response_buffer, ":");
+        if (date_and_time->tm_sec < 10) {
+            strcat(*response_buffer, "0");
+        }
+        n = sprintf(int_string, "%d", date_and_time->tm_sec);
+        strcat(*response_buffer, int_string);
+        strcat(*response_buffer, " GMT\r\n");
+
+        // Content Type
+        int i;
+        for (i=0; i < sizeof(file_name); i++) {
+            if (file_name[i] == '.') {
+                switch (file_name[i+1]) {
+                case 'j':
+                    strcat(*response_buffer, "Content-Type: image/jpeg\r\n");
+                    break;
+                case 'g':
+                    strcat(*response_buffer, "Content-Type: image/gif\r\n");
+                    break;
+                default:
+                    strcat(*response_buffer, "Content-Type: text/html; charset=utf-8\r\n");
+                    break;
+                }
+                break;
+            }
+        }
+        if (i == sizeof(file_name)) {    // No extension
+            strcat(*response_buffer, "Content-Type: text/html; charset=utf-8\r\n");
+        }
         
+        if (access(file_name, R_OK) == -1) {        // Permission Error
+            // Content-Length
+            strcat(*response_buffer, "Content-Length: 89\r\n");
+            // Data
+            strcat(*response_buffer, "\r\n<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD><BODY><H1>403 Forbidden</H1></BODY></HTML>");
+            return NULL;
+        }
+        else {
+            // Get File Info
+            struct stat attrib;
+            int ret_val;
+            ret_val = stat(file_name, &attrib);        
+
+            // Content-Length
+            char file_size[20];
+            n = sprintf(file_size, "%d", attrib.st_size);
+            strcat(*response_buffer, "Content-Length: ");
+            strcat(*response_buffer, file_size);
+            strcat(*response_buffer, "\r\n");
+
+            // Last-Modified
+            date_and_time = gmtime(&(attrib.st_mtime));
+            strcat(*response_buffer, "Last-Modified: ");
+            // Day of the week
+            switch (date_and_time->tm_wday) {
+            case 0:
+                strcat(*response_buffer, "Sun, ");
+                break;
+            case 1:
+                strcat(*response_buffer, "Mon, ");
+                break;
+            case 2:
+                strcat(*response_buffer, "Tue, ");
+                break;
+            case 3:
+                strcat(*response_buffer, "Wed, ");
+                break;
+            case 4:
+                strcat(*response_buffer, "Thu, ");
+                break;
+            case 5:
+                strcat(*response_buffer, "Fri, ");
+                break;
+            case 6:
+                strcat(*response_buffer, "Sat, ");
+                break;
+            }
+            // Date of the month
+            if (date_and_time->tm_mday < 10) {
+                strcat(*response_buffer, "0");
+            }
+            n = sprintf(int_string, "%d", date_and_time->tm_mday);
+            strcat(*response_buffer, int_string);  // change second argument to string
+            // Month
+            switch (date_and_time->tm_mon) {
+            case 0:
+                strcat(*response_buffer, " Jan ");
+                break;
+            case 1:
+                strcat(*response_buffer, " Feb ");
+                break;
+            case 2:
+                strcat(*response_buffer, " Mar ");
+                break;
+            case 3:
+                strcat(*response_buffer, " Apr ");
+                break;
+            case 4:
+                strcat(*response_buffer, " May ");
+                break;
+            case 5:
+                strcat(*response_buffer, " Jun ");
+                break;
+            case 6:
+                strcat(*response_buffer, " Jul ");
+                break;
+            case 7:
+                strcat(*response_buffer, " Aug ");
+                break;
+            case 8:
+                strcat(*response_buffer, " Sep ");
+                break;
+            case 9:
+                strcat(*response_buffer, " Oct ");
+                break;
+            case 10:
+                strcat(*response_buffer, " Nov ");
+                break;
+            case 11:
+                strcat(*response_buffer, " Dec ");
+                break;
+            }
+            // Year
+            n = sprintf(int_string, "%d", date_and_time->tm_year + 1900);  // Epoch time
+            strcat(*response_buffer, int_string);
+            // Hour
+            strcat(*response_buffer, " ");
+            if (date_and_time->tm_hour < 10) {
+                strcat(*response_buffer, "0");
+            }
+            n = sprintf(int_string, "%d", date_and_time->tm_hour);
+            strcat(*response_buffer, int_string);
+            // Minutes
+            strcat(*response_buffer, ":");
+            if (date_and_time->tm_min < 10) {
+                strcat(*response_buffer, "0");
+            }
+            n = sprintf(int_string, "%d", date_and_time->tm_min);
+            strcat(*response_buffer, int_string);
+            // Seconds
+            strcat(*response_buffer, ":");
+            if (date_and_time->tm_sec < 10) {
+                strcat(*response_buffer, "0");
+            }
+            n = sprintf(int_string, "%d", date_and_time->tm_sec);
+            strcat(*response_buffer, int_string);
+            strcat(*response_buffer, " GMT\r\n");
+
+            // Data
+//             FILE *fp;
+//             fp = fopen(file_name, 'r');
+//             strcat(*response_buffer, "\r\n<HTML><HEAD><TITLE>WORKS</TITLE></HEAD><BODY><H1>WORKS</H1></BODY></HTML>");
+            *rb_len = attrib.st_size;
+            return file_name;
+        }
     }
 }
 
@@ -377,29 +577,36 @@ int main(int argc, char *argv[])
     
     int n;
     char buffer[256];
-    char *response_buffer;
-    response_buffer = (char*) malloc(1000); 
-    int rb_len = 0;
+    char *response_buffer, *file_name;
+    response_buffer = (char*) malloc(1000);
+    int rb_len = 0, data_len = 0;
     
     memset(buffer, 0, 256);  //reset memory
     
     //read client's message
     n = read(newsockfd,buffer,255);
     if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
+    printf("Here is the message:\n%s\n",buffer);
 
-    //printSomething();
-    
     //reply to client
-    parse(buffer, &response_buffer);    // need & anywhere?
-    char c = response_buffer[rb_len];
-    while (c != NULL)
-    {
-        rb_len++;
-        c = response_buffer[rb_len];
-    }
-    n = write(newsockfd, response_buffer, rb_len);
+    file_name = parse(buffer, &response_buffer, &rb_len);    // need & anywhere?
+//     char c = response_buffer[rb_len];
+//     while (c != NULL)
+//     {
+//         rb_len++;
+//         c = response_buffer[rb_len];
+//     }
+//     n = write(newsockfd, response_buffer, rb_len);
+    n = write(newsockfd, response_buffer, 1000);
     if (n < 0) error("ERROR writing to socket");
+    printf("\n\n%d %s\n\n", rb_len, file_name);
+    
+    if (file_name != NULL) {
+        FILE *fp;
+        fp = fopen(file_name, 'r');
+        n = write(newsockfd, fp, rb_len);
+        if (n < 0) error("ERROR writing to socket");
+    }
     
     
     close(newsockfd);//close connection 
